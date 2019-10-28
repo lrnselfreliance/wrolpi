7a436e747a45d8189df4a09546280404108d160b
Roland <roland@learningselfreliance.com>
2019-10-28 15:24:30 -0600

    Using echos to document install progress.

--- wrolpi/install.sh
+++ wrolpi/install.sh
@@ -4,40 +4,42 @@
 
 set -x
 
-# Get the latest WROLPi code.
+echo "Installing git"
+apt update
+apt install -y git
+
+echo "Getting the latest WROLPi code"
+git --version
 git clone https://github.com/lrnselfreliance/wrolpi.git /opt/wrolpi || (cd /opt/wrolpi && git pull origin master)
 cd /opt/wrolpi || exit 1
 
-# Remove any old versions of docker.
+echo "Remove any old versions of docker"
 apt-get remove docker docker-engine docker.io containerd runc
-# Install docker's dependencies
-apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
-# Get docker's apt key
+echo "Installing docker repo and keys"
+apt install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-# Add docker apt repo
 add-apt-repository "deb [arch=arm64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt update
-# Install docker -ce
+echo "Installing docker-ce & docker-compose"
 apt install docker-ce docker-ce-cli containerd.io
-# Get docker-compose
 curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
-# Install Python 3.7
-apt install python3.7 python3.7-dev python3.7-doc python3.7-venv
+echo "Installing Python 3.7"
+apt install -y python3.7 python3.7-dev python3.7-doc python3.7-venv
 # Setup the virtual environment that main.py expects
 python3 -m venv /opt/wrolpi/venv
 . /opt/wrolpi/venv/bin/activate
 
-# Install python requirements
+echo "Installing python requirements files"
 shopt -s globstar nullglob
 for req in /opt/wrolpi/**/requirements.txt
 do
   pip install -r "${req}"
 done
 
-# Build docker containers
+echo "Building docker containers"
 docker-compose build --parallel
 
-# Start WROLPi
+echo "Starting WORLPi"
 docker-compose up -d

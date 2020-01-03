4e4fd6dd1b007e2ee0209a4eea67877cb0ca7765
Roland <roland@learningselfreliance.com>
2020-01-03 14:52:19 -0700

    Moving description getting below static code.

--- app/src/components/VideoPlayer.js
+++ app/src/components/VideoPlayer.js
@@ -5,17 +5,17 @@ import {VIDEOS_API} from "./Common";
 function Video(props) {
     let video_hash = props.video.video_path_hash;
 
-    let description = '';
-    if (props.video.info_json) {
-        let info_json = JSON.parse(props.video.info_json);
-        description = info_json['description'];
-    }
-
     let poster_url = VIDEOS_API + '/static/poster/' + video_hash;
     let video_url = VIDEOS_API + '/static/video/' + video_hash;
     let captions_url = VIDEOS_API + '/static/caption/' + video_hash;
 
     let video_download_url = video_url + '?download=true';
+
+    let description = 'No description available.';
+    if (props.video.info_json) {
+        let info_json = JSON.parse(props.video.info_json);
+        description = info_json['description'];
+    }
 
     return (
         <>

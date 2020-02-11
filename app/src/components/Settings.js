0bb63cf5236d87bd702f5743ad761c9b0efb08ec
Roland <roland@learningselfreliance.com>
2020-02-10 22:26:01 -0700

    Reworked settings page without bootstrap.

--- app/src/components/Settings.js
+++ app/src/components/Settings.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
 import {API_URI} from "./Common"
+import {Button, Container, Form, Header} from "semantic-ui-react";
 
 async function getConfig() {
     let url = `http://${API_URI}/api/settings`;
-    console.log('url', url);
     let response = await fetch(url);
     let data = await response.json();
     return data['config'];
@@ -40,33 +38,39 @@ class Settings extends React.Component {
 
     render() {
         return (
-            <div className="container-fluid">
-                <h1>Settings</h1>
-                <p className="lead">
+            <Container style={{'marginTop': '2em'}}>
+                <Header as="h1" dividing>
+                    Settings
+                </Header>
+                <p>
                     The global settings for your server.
                 </p>
                 <p className="text-muted">
                     When saved, these will be written to your <i>local.yaml</i> file. Storing these settings in your
-                    local
-                    configuration file (rather than the database) allows your WROLPi to be rebuilt and save your
+                    local configuration file (rather than the database) allows your WROLPi to be rebuilt and save your
                     settings.
                 </p>
                 <Form id="settings" onSubmit={this.handleSubmit}>
-                    <Form.Group controlId="media_directory">
-                        <Form.Label>Media Directory</Form.Label>
-                        <Form.Control type="text" placeholder="/some/absolute/path" ref={this.mediaDirectory}/>
-                        <Form.Text className="text-muted">
-                            <b>This directory must already exist</b>.
-                            The directory in which your media will be stored. Typically, this will be some external
-                            drive like <i>/media/8TB</i>.
-                        </Form.Text>
-                    </Form.Group>
+                    <Form.Field>
+                        <label>Media Directory</label>
+                        <input
+                            type="text"
+                            placeholder="/some/absolute/path"
+                            ref={this.mediaDirectory}
+                        />
+                    </Form.Field>
 
-                    <Button variant="primary" type="submit">
+                    <p>
+                        <b>This directory must already exist</b>.
+                        The directory in which your media will be stored. Typically, this will be some external
+                        drive like <i>/media/8TB</i>.
+                    </p>
+
+                    <Button color="blue" type="submit">
                         Save
                     </Button>
                 </Form>
-            </div>
+            </Container>
         )
     }
 }

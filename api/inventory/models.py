eab793fb0da182f42c1db896ee4f81378f1c085f
Roland <roland@learningselfreliance.com>
2021-06-29 21:52:14 -0600

    Removing large dict methods in favor of using repr hack.

--- api/inventory/models.py
+++ api/inventory/models.py
@@ -31,25 +31,6 @@ class Item(Base):
         return f'<Item(id={self.id}, name={self.name}, brand={self.brand}, ' \
                f'count={self.count}, item_size={self.item_size}, unit={self.unit}, inventory={self.inventory_id})>'
 
-    def dict(self):
-        d = dict(
-            id=self.id,
-            brand=self.brand,
-            category=self.category,
-            count=self.count,
-            created_at=self.created_at,
-            deleted_at=self.deleted_at,
-            expiration_date=self.expiration_date,
-            inventory_id=self.inventory_id,
-            item_size=self.item_size,
-            name=self.name,
-            purchase_date=self.purchase_date,
-            serving=self.serving,
-            subcategory=self.subcategory,
-            unit=self.unit,
-        )
-        return d
-
 
 class Inventory(Base):
     __tablename__ = 'inventory'
@@ -66,13 +47,6 @@ class Inventory(Base):
         return f'<Inventory(id={self.id}, name={self.name!r}, deleted={self.deleted_at})>'
 
     def dict(self):
-        # TODO surely there is a better way to do this?
-        d = dict(
-            id=self.id,
-            name=self.name,
-            viewed_at=self.viewed_at,
-            created_at=self.created_at,
-            deleted_at=self.deleted_at,
-            items=[i.dict() for i in self.items]
-        )
+        d = super().dict()
+        d['items'] = [i.dict() for i in self.items]
         return d

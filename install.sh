44720327f83ec1cfaa38951c388ddf0037228fd6
Roland <roland@learningselfreliance.com>
2021-10-20 21:44:33 -0600

    Always installing curl.

--- install.sh
+++ install.sh
@@ -16,7 +16,8 @@ set -e
 
 # Install git
 apt update
-apt install -y git raspberrypi-kernel-headers
+apt install -y git raspberrypi-kernel-headers apt-transport-https ca-certificates curl gnupg-agent \
+  software-properties-common
 
 # Get the latest WROLPi code
 git --version
@@ -48,8 +49,7 @@ apt-get remove docker docker-engine docker.io containerd runc || : # ignore fail
 # Installing docker repo and keys
 [ -f /etc/apt/sources.list.d/wrolpi-docker.list ] || (
   # Docker repo not installed, install it.  Update the package list with these new repos.
-  apt install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common &&
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add - &&
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add - &&
     echo "deb [arch=armhf] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list &&
     apt update
 )

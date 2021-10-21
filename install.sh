eceb673e6a24647257bfe9d38490e85a61992e12
Roland <roland@learningselfreliance.com>
2021-10-20 21:38:37 -0600

    Fixing pip install command.

--- install.sh
+++ install.sh
@@ -38,7 +38,7 @@ python3 -m venv /opt/wrolpi/venv
 . /opt/wrolpi/venv/bin/activate
 
 # Install python requirements files
-pip3 install /opt/wrolpi/requirements.txt
+pip3 install -r /opt/wrolpi/requirements.txt
 
 # Any further pip commands will be global
 deactivate

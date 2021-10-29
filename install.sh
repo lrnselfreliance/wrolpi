3504d56c3aead068ad732b572801aab1e92e11a1
Roland <roland@learningselfreliance.com>
2021-10-29 13:38:40 -0600

    Using command -v because it is universal.

--- install.sh
+++ install.sh
@@ -88,7 +88,7 @@ cp /opt/wrolpi/nginx.conf /etc/nginx/nginx.conf
 /usr/sbin/nginx -s reload
 
 # Create the WROLPi user
-grep wrolpi /etc/passwd || useradd -d /opt/wrolpi wrolpi -s "$(which bash)"
+grep wrolpi /etc/passwd || useradd -d /opt/wrolpi wrolpi -s "$(command -v bash)"
 chown -R wrolpi:wrolpi /opt/wrolpi
 
 # Install the systemd services

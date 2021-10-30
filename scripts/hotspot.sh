a6fa1a9afbcde1342f0e8975e1c9ab0510388c02
Roland <roland@learningselfreliance.com>
2021-10-30 17:09:50 -0600

    Using NetworkManager for hotspot.

--- scripts/hotspot.sh
+++ scripts/hotspot.sh
@@ -3,68 +3,33 @@
 set -x
 set -e
 
-apt install -y hostapd isc-dhcp-server
+cat >/etc/NetworkManager/system-connections/Hotspot.nmconnection <<'EOF'
+[connection]
+id=Hotspot
+uuid=f8309ae2-cb53-480d-a921-fea698fb8ac8
+type=wifi
+permissions=
 
-# hostapd will broadcast the hotspot
-cat >/etc/hostapd/hostapd.conf <<'EOF'
-# the interface used by the AP
-interface=wlan0
-driver=nl80211
-hw_mode=g
-# the channel to use
-channel=1
-# limit the frequencies used to those allowed in the country
-ieee80211d=1
-# the country code
-country_code=US
-# 802.11n support
-ieee80211n=1
-# QoS support
-wmm_enabled=1
-# the name of the AP
+[wifi]
+mac-address-blacklist=
+mode=ap
 ssid=WROLPi
-macaddr_acl=0
-# 1=wpa, 2=wep, 3=both
-auth_algs=1
-ignore_broadcast_ssid=0
-# WPA2 only
-wpa=2
-wpa_passphrase=wrolpihotspot
-wpa_key_mgmt=WPA-PSK
-rsn_pairwise=CCMP
+
+[wifi-security]
+key-mgmt=wpa-psk
+psk=wrolpihotspot
+
+[ipv4]
+dns-search=
+method=shared
+
+[ipv6]
+addr-gen-mode=stable-privacy
+dns-search=
+ip6-privacy=0
+method=shared
+
+[proxy]
 EOF
 
-# Serve DHCP on the hotspot interface.
-cat >/etc/dhcp/dhcpd.conf <<'EOF'
-default-lease-time 600;
-max-lease-time 7200;
-
-subnet 192.168.0.0 netmask 255.255.255.0 {
- range 192.168.0.100 192.168.0.200;
- option routers 192.168.0.1;
- option domain-name-servers 192.168.0.1, 192.168.0.2;
- option domain-name "wrolpi.local";
-}
-EOF
-sed -ie 's/INTERFACESv4.*/INTERFACESv4="wlan0"/' /etc/default/isc-dhcp-server
-
-# Configure eth0 as a normal dhcp interface, wlan0 as our hotspot interface.
-cat >/etc/netplan/10-wrolpi-hotspot.yaml <<'EOF'
-network:
-  version: 2
-  renderer: networkd
-  ethernets:
-    eth0:
-      dhcp4: yes
-      optional: yes
-    wlan0:
-      dhcp4: no
-      dhcp6: no
-      optional: yes
-      addresses:
-        - 192.168.0.1/24
-
-EOF
-
-netplan generate && netplan apply && systemctl unmask hostapd.service &&
-  systemctl restart hostapd.service isc-dhcp-server.service
+systemctl restart NetworkManager.service

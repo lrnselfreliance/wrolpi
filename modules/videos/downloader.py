d3db8ee9835dc1d5e0f5505bfc29ce94af4e807a
Roland <roland@learningselfreliance.com>
2022-01-14 15:40:13 -0700

    Adding 22 to preferred video format for odd 720p resolution.

--- modules/videos/downloader.py
+++ modules/videos/downloader.py
@@ -37,7 +37,7 @@ ChannelIEs = {
 }
 
 PREFERRED_VIDEO_EXTENSION = 'mp4'
-PREFERRED_VIDEO_FORMAT = 'best[height=720],mp4-480p,mp4-360p,mp4-240p'
+PREFERRED_VIDEO_FORMAT = 'best[height=720],22,720p,mp4-480p,mp4-360p,mp4-240p'
 
 
 def get_no_channel_directory():

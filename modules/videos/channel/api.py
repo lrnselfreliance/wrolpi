8bfa2cdba9721513a4511f55a4fc530656e3bb34
StruckCroissant <32440863+StruckCroissant@users.noreply.github.com>
2022-02-07 13:47:17 -0600

    Update api.py

--- modules/videos/channel/api.py
+++ modules/videos/channel/api.py
@@ -52,7 +52,6 @@ def channel_post(_: Request, body: schema.ChannelPostRequest):
     bd_edit = body.__dict__
     bd_edit['name'] = bd_edit['name'].strip()
     channel = lib.create_channel(bd_edit)  # TODO don't use the dataclass as a dict.
-    print(channel)
 
     # Refresh the videos asynchronously
     from ..api import refresh_videos

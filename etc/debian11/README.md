eca3acb8e5ca79d944e3ce7be84dd7979afe5eed
Roland <roland@learningselfreliance.com>
2022-03-19 15:50:50 -0600

    Moving install configs into etc directory.

---
+++ etc/debian11/README.md
@@ -0,0 +1,1 @@
+The files in `etc` are files necessary to build an on-the-metal WROLPi.
--- scripts/install_map_ubuntu_20.04.sh
+++ scripts/install_map_ubuntu_20.04.sh
@@ -85,133 +85,16 @@ fi
 
 set -e
 
-cat >/usr/local/etc/renderd.conf <<'EOF'
-[renderd]
-num_threads=4
-tile_dir=/var/lib/mod_tile
-stats_file=/var/run/renderd/renderd.stats
-
-[mapnik]
-plugins_dir=/usr/lib/mapnik/3.0/input
-font_dir=/usr/share/fonts/truetype
-font_dir_recurse=1
-
-[ajt]
-URI=/hot/
-TILEDIR=/var/lib/mod_tile
-XML=/opt/openstreetmap-carto/mapnik.xml
-HOST=localhost
-TILESIZE=256
-MAXZOOM=20
-EOF
-
+cp /opt/wrolpi/etc/ubuntu20.04/rendred.conf /usr/local/etc/renderd.conf
 # Enable mod_tile.
-cat >>/etc/apache2/conf-available/mod_tile.conf <<'EOF'
-LoadModule tile_module /usr/lib/apache2/modules/mod_tile.so
-EOF
+cp /opt/wrolpi/etc/ubuntu20.04/mod_tile.conf /etc/apache2/conf-available/mod_tile.conf
+
 /usr/sbin/a2enconf mod_tile
 
 # Configure Apache2 to listen on 8084.
-cat >/etc/apache2/ports.conf <<'EOF'
-# If you just change the port or add more ports here, you will likely also
-# have to change the VirtualHost statement in
-# /etc/apache2/sites-enabled/000-default.conf
-
-Listen 8084
-
-<IfModule ssl_module>
-	Listen 443
-</IfModule>
-
-<IfModule mod_gnutls.c>
-	Listen 443
-</IfModule>
-
-# vim: syntax=apache ts=4 sw=4 sts=4 sr noet
-EOF
-
-cat >/etc/apache2/sites-available/000-default.conf <<'EOF'
-<VirtualHost *:8084>
-	# The ServerName directive sets the request scheme, hostname and port that
-	# the server uses to identify itself. This is used when creating
-	# redirection URLs. In the context of virtual hosts, the ServerName
-	# specifies what hostname must appear in the request's Host: header to
-	# match this virtual host. For the default virtual host (this file) this
-	# value is not decisive as it is used as a last resort host regardless.
-	# However, you must set it for any further virtual host explicitly.
-	#ServerName www.example.com
-
-	ServerAdmin webmaster@localhost
-
-	LoadTileConfigFile /usr/local/etc/renderd.conf
-	ModTileRenderdSocketName /var/run/renderd/renderd.sock
-	# Timeout before giving up for a tile to be rendered
-	ModTileRequestTimeout 0
-	# Timeout before giving up for a tile to be rendered that is otherwise missing
-	ModTileMissingRequestTimeout 30
-
-	DocumentRoot /var/www/html
-
-	# Available loglevels: trace8, ..., trace1, debug, info, notice, warn,
-	# error, crit, alert, emerg.
-	# It is also possible to configure the loglevel for particular
-	# modules, e.g.
-	#LogLevel info ssl:warn
-
-	ErrorLog ${APACHE_LOG_DIR}/error.log
-	CustomLog ${APACHE_LOG_DIR}/access.log combined
-
-	# For most configuration files from conf-available/, which are
-	# enabled or disabled at a global level, it is possible to
-	# include a line for only one particular virtual host. For example the
-	# following line enables the CGI configuration for this host only
-	# after it has been globally disabled with "a2disconf".
-	#Include conf-available/serve-cgi-bin.conf
-</VirtualHost>
-
-# vim: syntax=apache ts=4 sw=4 sts=4 sr noet
-EOF
-
-cat >/var/www/html/index.html <<'EOF'
-<!DOCTYPE HTML>
-<html lang="en">
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="/leaflet.js"></script>
-    <link rel="stylesheet" href="/leaflet.css" />
-    <style>
-      html, body {
-        height: 100%;
-        padding: 0;
-        margin: 0;
-      }
-      #map {
-        /* configure the size of the map */
-        width: 100%;
-        height: 100%;
-      }
-    </style>
-  </head>
-  <body>
-    <div id="map"></div>
-    <script>
-      // initialize Leaflet
-      var map = L.map('map').setView({lon: 0, lat: 0}, 2);
-
-      // add the OpenStreetMap tiles
-      L.tileLayer(`http://${window.location.hostname}:8084/hot/{z}/{x}/{y}.png`, {
-        maxZoom: 19,
-        attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
-      }).addTo(map);
-
-      // show the scale bar on the lower left corner
-      L.control.scale({imperial: true, metric: true}).addTo(map);
-    </script>
-  </body>
-</html>
-EOF
-
+cp /opt/wrolpi/etc/ubuntu20.04/ports.conf /etc/apache2/ports.conf
+cp /opt/wrolpi/etc/ubuntu20.04/000-default.conf /etc/apache2/sites-available/000-default.conf
+cp /opt/wrolpi/etc/ubuntu20.04/index.html /var/www/html/index.html
 # Copy Leaflet files to Apache's directory so they can be used offline.
 cp /opt/wrolpi/modules/map/leaflet.js /var/www/html/leaflet.js
 cp /opt/wrolpi/modules/map/leaflet.css /var/www/html/leaflet.css

d2fbf12e266b915b3bcf9d93975b6a99776f4006
Roland <roland@learningselfreliance.com>
2022-11-05 14:38:33 -0600

    Using worst-case-scenario for calculating map time-to-import.

--- modules/map/test/test_models.py
+++ modules/map/test/test_models.py
@@ -5,14 +5,14 @@ import pytest
 from modules.map import models
 
 
-@pytest.mark.parametrize('path,size,expected', [
-    ('file.osm.pbf', 0, 0),
-    ('file.osm.pbf', -1, 0),
-    ('file.osm.pbf', 17737381, 4498),
-    ('file.osm.pbf', 63434267, 16087),
-    ('file.osm.pbf', 87745484, 22253),
-    ('file.osm.pbf', 136372996, 34586),
-    ('file.osm.pbf', 116318111, 29499),
+@pytest.mark.parametrize('size,expected', [
+    (0, 0),
+    (-1, 0),
+    (17737381, 4498),
+    (63434267, 16087),
+    (87745484, 22253),
+    (136372996, 34586),
+    (116318111, 29499),
 ])
-def test_seconds_to_import_rpi4(path, size, expected):
-    assert models.seconds_to_import(Path(path), size) == expected
+def test_seconds_to_import_rpi4(size, expected):
+    assert models.seconds_to_import(Path('file.osm.pbf'), size) == expected

1d9e573c922c2d3efc2ce2331541e10b6d4146c6
Roland <roland@learningselfreliance.com>
2022-12-28 18:30:46 -0700

    Handling .hgt files mimetype specifically.

--- wrolpi/files/lib.py
+++ wrolpi/files/lib.py
@@ -123,6 +123,8 @@ def _mimetype_suffix_map(path: Path, mimetype: str):
             return 'text/srt'
         if suffix.endswith('.yaml') or suffix.endswith('.yml'):
             return 'text/yaml'
+        if suffix.endswith('.hgt'):
+            return 'application/octet-stream'
     return mimetype
 
 

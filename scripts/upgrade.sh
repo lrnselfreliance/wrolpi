7ff2351dc521a67c8b14ae86a69cd15bbc602d47
Roland <roland@learningselfreliance.com>
2023-05-01 13:00:23 -0600

    Updating nginx configs during upgrade.

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -2,3 +2,8 @@
 
 # Upgrade the API and App.
 /bin/bash /opt/wrolpi/scripts/build_api_and_app.sh
+
+# Update nginx configs
+cp /opt/wrolpi/nginx.conf /etc/nginx/nginx.conf
+cp /opt/wrolpi/50x.html /var/www/50x.html
+/usr/sbin/nginx -s reload

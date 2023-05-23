f764cee44ea74f6a392b9882fc3a43f03571c714
Roland <roland@learningselfreliance.com>
2023-05-23 16:41:48 -0600

    Upgrade systemd file before building api.

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -1,5 +1,9 @@
 #! /usr/bin/env bash
 
+# Update systemd files.
+cp /opt/wrolpi/etc/raspberrypios/wrolpi-api.service /etc/systemd/system/
+/usr/bin/systemctl daemon-reload
+
 # Upgrade the API and App.
 /bin/bash /opt/wrolpi/scripts/build_api_and_app.sh
 
@@ -7,7 +11,3 @@
 cp /opt/wrolpi/nginx.conf /etc/nginx/nginx.conf
 cp /opt/wrolpi/50x.html /var/www/50x.html
 /usr/sbin/nginx -s reload
-
-# Update systemd files.
-cp /opt/wrolpi/etc/raspberrypios/wrolpi-api.service /etc/systemd/system/
-/usr/bin/systemctl daemon-reload

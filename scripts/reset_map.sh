ca9c24738fd76a047c4afc128e34b795b66fe395
Roland <roland@learningselfreliance.com>
2023-09-25 17:56:42 -0600

    Allowing install.sh to install map configs so map is initialized for Debian 12.

--- scripts/reset_map.sh
+++ scripts/reset_map.sh
@@ -14,26 +14,11 @@ MAX_CACHE=$((RAM_KB / 1024 / 4))
 
 yes_or_no "Are you sure you want to delete the map database and cache?  This can't be undone!" || exit 0
 
-if [[ ! -f /usr/lib/apache2/modules/mod_tile.so ||
-  ! -f /opt/openstreetmap-carto/mapnik.xml ||
-  ! -s /opt/openstreetmap-carto/mapnik.xml ||
-  ! -f /opt/openstreetmap-carto/scripts/get-external-data.py ||
-  ! -f /opt/openstreetmap-carto/openstreetmap-carto.style ||
-  ! -f /opt/openstreetmap-carto/indexes.sql ]]; then
-  echo "The map has not been installed.  Run install.sh"
-  exit 2
-fi
-
 set -e
 set -x
 
 systemctl stop renderd
 
-if [ -d /var/lib/mod_tile ]; then
-  rm -rf /var/lib/mod_tile/*
-else
-  mkdir /var/lib/mod_tile
-fi
 chown -R _renderd:_renderd /var/lib/mod_tile
 
 sudo -u postgres dropdb gis || :
@@ -43,5 +28,7 @@ sudo -u postgres dropuser _renderd || :
 sudo -u postgres psql -d wrolpi -c "UPDATE map_file SET imported=false"
 
 yes | bash "${PROJECT_DIR}/scripts/initialize_map_db.sh"
+
+set +x
 
 echo "Map has been reset."

2c36ed8010fb2c871b0e83e9b4a348321e8a76d4
Roland <roland@learningselfreliance.com>
2023-09-25 20:12:30 -0600

    Creating mapnik.xml file during debian build.

--- scripts/reset_map.sh
+++ scripts/reset_map.sh
@@ -29,6 +29,25 @@ sudo -u postgres psql -d wrolpi -c "UPDATE map_file SET imported=false"
 
 yes | /opt/wrolpi/scripts/initialize_map_db.sh
 
+# Initialize gis database.
+[ -d /opt/openstreetmap-carto ] && chown -R wrolpi:wrolpi /opt/openstreetmap-carto
+git clone https://github.com/lrnselfreliance/openstreetmap-carto.git /opt/openstreetmap-carto || :
+chown -R _renderd:_renderd /opt/openstreetmap-carto
+sudo -u _renderd /bin/bash -c '(cd /opt/openstreetmap-carto && git fetch && git checkout master && git reset --hard origin/master && git pull --ff)'
+if [[ ! -f /opt/openstreetmap-carto/mapnik.xml || ! -s /opt/openstreetmap-carto/mapnik.xml ]]; then
+  (cd /opt/openstreetmap-carto && carto project.mml >/opt/openstreetmap-carto/mapnik.xml)
+fi
+chown -R _renderd:_renderd /opt/openstreetmap-carto
+
+# Reset map.  Force it.
+/opt/wrolpi/scripts/reset_map.sh -f
+
+cp /opt/wrolpi/etc/raspberrypios/renderd.conf /etc/renderd.conf
+# Enable mod_tile.
+cp /opt/wrolpi/etc/raspberrypios/mod_tile.conf /etc/apache2/conf-available/mod_tile.conf
+
+/usr/sbin/a2enconf mod_tile
+
 set +x
 
 echo "Map has been reset."

eabe517b89e44412ce16baf09b8df25761594de7
Roland <roland@learningselfreliance.com>
2024-12-20 11:14:01 -0700

    Fixing Files link to nginx media file browser.

--- app/src/components/Vars.js
+++ app/src/components/Vars.js
@@ -7,8 +7,8 @@ export const DEFAULT_LIMIT = 20;
 export const NAME = process.env && process.env.REACT_APP_NAME ? process.env.REACT_APP_NAME : null;
 
 // Other services on a WROLPi.
-const port = window.location.port ? `${window.location.port}` : '';
-export const FILES_MEDIA_URI = `https://${window.location.hostname}${port}/media/`;
+const port = window.location.port ? `${window.location.port.replace(/^/, '')}` : '';
+export const FILES_MEDIA_URI = `https://${window.location.hostname}:${port}/media/`;
 export const MAP_VIEWER_URI = `https://${window.location.hostname}:8084`;
 export const ZIM_VIEWER_URI = `https://${window.location.hostname}:8085`;
 export const HELP_VIEWER_URI = `https://${window.location.hostname}:8086`;

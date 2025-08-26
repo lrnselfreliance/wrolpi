cd2fdce90a264571ef0614984af85dc9a8da5b0a
Roland <roland@learningselfreliance.com>
2025-08-26 16:24:42 -0600

    Adding best matching video resolutions at the start of video download format lists.

--- modules/videos/downloader.py
+++ modules/videos/downloader.py
@@ -42,17 +42,20 @@ logger = logger.getChild(__name__)
 
 VIDEO_RESOLUTION_MAP = {
     '360p': [
+        'bestvideo[height=360]+bestaudio/best[height=360]',
         '134+bestaudio', '230+bestaudio', 'mp4-360p', 'res:360',
         'hls-175+bestaudio',  # 360p avc1 + best audio
         '396+bestaudio', '243+bestaudio',  # Vertical
         'hls-655+bestaudio', 'hls-655',
     ],
     '480p': [
+        'bestvideo[height=480]+bestaudio/best[height=480]',
         '135+bestaudio', '231+bestaudio', '135+139', 'mp4-480p', 'res:480',
         'hls-312+bestaudio',  # 480p avc1 + best audio
         '788+bestaudio', '780+bestaudio', '397+bestaudio',  # Vertical
     ],
     '720p': [
+        'bestvideo[height=720]+bestaudio/best[height=720]',
         '298+bestaudio', '232+bestaudio',  # 720@60 avc1 + best audio
         '136+bestaudio', '22', '311+bestaudio', 'res:720',
         'hls-517+bestaudio', 'hls-637+bestaudio',  # 720p avc1 + best audio
@@ -60,6 +63,7 @@ VIDEO_RESOLUTION_MAP = {
         'hls-2890+bestaudio', 'hls-2890',
     ],
     '1080p': [
+        'bestvideo[height=1080]+bestaudio/best[height=1080]',
         '299+bestaudio', '312+bestaudio', '270+bestaudio',  # 1080@60 avc1 + best audio
         '137+bestaudio', '614+bestaudio', 'res:1080',
         'hls-899+bestaudio', 'hls-1271+bestaudio',  # 1080p avc1 + best audio
@@ -67,10 +71,12 @@ VIDEO_RESOLUTION_MAP = {
         'hls-4026+bestaudio', 'hls-4026',
     ],
     '1440p': [
+        'bestvideo[height=1440]+bestaudio/best[height=1440]',
         '639+bestaudio',  # vp9, best audio, hdr
         '623+bestaudio',  # vp9, best audio, no hdr
         '620+bestaudio', 'res:1440'],
     '2160p': [
+        'bestvideo[height=2160]+bestaudio/best[height=2160]',
         '642+bestaudio',  # vp9, best audio, hdr
         '628+bestaudio',  # vp9, best audio, no hdr
         '625+bestaudio', 'res:2160'],

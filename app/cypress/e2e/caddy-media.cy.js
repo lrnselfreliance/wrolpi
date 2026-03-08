da8a9bb92316e9ca55df752f17c3c34a7515cf04
Roland <roland@learningselfreliance.com>
2026-03-08 09:54:00 -0600

    Caddy must serve files inline on /media and download on /download.

---
+++ app/cypress/e2e/caddy-media.cy.js
@@ -0,0 +1,27 @@
+describe('Caddy Media Serving Tests', () => {
+
+    describe('/media/ Content-Disposition inline', () => {
+        it('sets Content-Disposition inline on /media/ file requests', () => {
+            cy.request('GET', '/media/config/wrolpi.yaml').then((response) => {
+                expect(response.status).to.eq(200);
+                expect(response.headers['content-disposition']).to.contain('inline');
+            });
+        });
+
+        it('serves .yaml files with text/yaml Content-Type', () => {
+            cy.request('GET', '/media/config/wrolpi.yaml').then((response) => {
+                expect(response.status).to.eq(200);
+                expect(response.headers['content-type']).to.contain('text/yaml');
+            });
+        });
+    });
+
+    describe('/download/ Content-Disposition attachment', () => {
+        it('sets Content-Disposition attachment on /download/ file requests', () => {
+            cy.request('GET', '/download/config/wrolpi.yaml').then((response) => {
+                expect(response.status).to.eq(200);
+                expect(response.headers['content-disposition']).to.contain('attachment');
+            });
+        });
+    });
+});
